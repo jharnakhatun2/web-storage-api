c92d900eef0e4048d312590fdb2f1040ba703721
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-22 20:30:20 +0600

    added localStorage exercise

---
+++ local.js
@@ -0,0 +1,20 @@
+function setUserName(key, value) {
+    localStorage.setItem(key, value);
+}
+function getUserName(key) {
+    alert(localStorage.getItem(key));
+}
+function setName(key, value) {
+    localStorage.setItem(key, value);
+}
+function getName(key) {
+    alert(localStorage.getItem(key));
+}
+
+function removeOneItem(key) {
+    localStorage.removeItem(key);
+}
+
+function clear() {
+    localStorage.clear();
+}
